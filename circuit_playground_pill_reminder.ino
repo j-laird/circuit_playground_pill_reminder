56e942c2edf7aacb8a9737ccd59b59b13b380668
j-laird <chaindrive@gmail.com>
2019-01-21 23:33:42 -0800

    Updated animations for elapsed time
    Tweaked 10h color slightly (to amber), added pale purple elapsed time
    (full ring = 24h) to show elapsed time at 10s interval

--- circuit_playground_pill_reminder.ino
+++ circuit_playground_pill_reminder.ino
@@ -9,6 +9,7 @@
 #define SECONDS_PER_HOUR (SECONDS_PER_MIN * MINUTES_PER_HOUR)
 #define SECONDS_PER_12H (SECONDS_PER_HOUR * 12ul) 
 #define SECONDS_PER_10H (SECONDS_PER_HOUR * 8ul)
+#define SECONDS_PER_PIXEL_24H (SECONDS_PER_HOUR * 24ul / 10ul)
 
 // we light one pixel at a time, this is our counter
 uint8_t pixeln = 0;
@@ -17,6 +18,7 @@ float x_accel;
 float y_accel;
 float z_accel;
 uint32_t loopsSinceBottleTip = 0;
+uint32_t hoursSinceBottleTip;
 
 const uint32_t pill_interval_second = SECONDS_PER_12H;
 const uint32_t pill_interval_first = SECONDS_PER_10H;
@@ -54,14 +56,29 @@ void loop() {
   {
     for(pixeln = 0; pixeln < 11; pixeln++)
     {
-      CircuitPlayground.setPixelColor(pixeln, 0,0,255);
+      CircuitPlayground.setPixelColor(pixeln, 255,255,255);
+      delay(100);
+      CircuitPlayground.clearPixels();
     }
   }
-  else if(loopsSinceBottleTip == 1)
+
+  // every 10 seconds, show elapsed time since medication by indicating in dim purple (1 pixel = 2.4h elapsed):
+  if(!(loopsSinceBottleTip % 10))
   {
+    for(pixeln=0; pixeln<10; pixeln++)
+      {
+        if(pixeln < (loopsSinceBottleTip/SECONDS_PER_PIXEL_24H))
+        {
+          CircuitPlayground.setPixelColor(pixeln, 16,0,16);
+        }
+      }
+    delay(1000);
     CircuitPlayground.clearPixels();
+    // we just burned an extra second, so add to time:
+    loopsSinceBottleTip++;
   }
-  
+
+  // show green animation if > 12h:
   if(loopsSinceBottleTip > pill_interval_second)
   {
     CircuitPlayground.clearPixels();
@@ -80,6 +97,8 @@ void loop() {
       }
     }
   }
+  
+  // show amber animation if 10-12h:
   else if(loopsSinceBottleTip > pill_interval_first)
   {
     CircuitPlayground.clearPixels();
@@ -87,20 +106,22 @@ void loop() {
     {
       for(pixeln = 0; pixeln <= 10; pixeln += 2)
       {
-        CircuitPlayground.setPixelColor(pixeln, 128,0,0);
+        CircuitPlayground.setPixelColor(pixeln, 128,128,0);
       }
     }
     else
     {
       for(pixeln = 1; pixeln <= 9; pixeln += 2)
       {
-        CircuitPlayground.setPixelColor(pixeln, 128,0,0);
+        CircuitPlayground.setPixelColor(pixeln, 128,128,0);
       }
     }
   }
 
-  Serial.print(loopsSinceBottleTip);
-  Serial.print("\n");
+  
+  // Debug elapsed time:
+  //Serial.print(loopsSinceBottleTip);
+  //Serial.print("\n");
 
   /************* TEST ACCEL */
   // Display the results (acceleration is measured in m/s*s)
